b90bc44be707da437c71a4b939464f17d965bae0
Rodrigo Rodrigues <rodrigo.rodrigues@compasso.com.br>
2022-10-05 22:37:00 -0300

    feature/RegRoutes: Adding Route Page Components

--- src/router.js
+++ src/router.js
@@ -1,23 +1,35 @@
 import { createRouter, createWebHistory }  from 'vue-router';
 
+// Coaches Components
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+
+// Requests Components
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+
+// Not Found Component
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
     // Browsing history
     history: createWebHistory(),
 
     routes: [
         { path: '/', redirect: '/coaches' },
-        { path: '/coaches', component: null },
+        { path: '/coaches', component: CoachesList },
         { 
             path: '/coaches/:id', 
-            component: null,
+            component: CoachDetail,
             children: [
                 // /coaches/:id/contact
-                { path: '/contact', component: null },
+                { path: '/contact', component: ContactCoach },
             ]
         },
-        { path: '/register', component: null },
-        { path: '/requests', component: null },
-        { path: '/:notFound(.*)', component: null },
+        { path: '/register', component: CoachRegistration },
+        { path: '/requests', component: RequestsReceived },
+        { path: '/:notFound(.*)', component: NotFound },
     ],
 });
 
