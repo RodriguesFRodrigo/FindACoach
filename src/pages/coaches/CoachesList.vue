645dd7647ba6a5d3205af840e4d259a090e6712f
Rodrigo Rodrigues <rodrigo.rodrigues@compasso.com.br>
2022-11-09 22:48:37 -0300

    working on list items

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -8,14 +8,24 @@
             <router-link to="/register">Register as Coach</router-link>
         </div>
         <ul v-if="hasCoaches">
-            <li v-for="coach in filteredCoaches" :key="coach.id">{{ coach.firstName }}</li>
+            <coach-item 
+              v-for="coach in filteredCoaches" 
+              :key="coach.id" 
+              :id="coach.id" 
+              :first-name="coach.firstName" 
+              :last-name="coach.lastName"
+              :rate="coach.hourlyRate"
+              :areas="coach.areas">
+            </coach-item>
         </ul>
         <h3 v-else>No coaches found.</h3>
     </section>
 </template>
 
 <script>
+import coachItem from '../../components/coaches/coachItem.vue';
   export default {
+  components: { coachItem },
     computed: {
         filteredCoaches() {
             //namespace/getter
@@ -27,3 +37,16 @@
     }
   }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
